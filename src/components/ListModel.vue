d5ebd51828dc676f57a32694b4fae966fda4fce5
pheamawas <pheamawas@gmail.com>
2025-03-13 15:48:06 +0700

    104-[add] ListModel.vue and some data

---
+++ src/components/ListModel.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    items:{
+        type: Array,
+        require: true
+    }
+})
+</script>
+ 
+<template>
+<div>
+    <div>
+        <slot name="header"></slot>
+    </div>
+    <div class="flex space-x-8 overflow-x-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none']">
+        <div v-for="(item, index) in items" :key="index" class="min-w-[170px]">
+            <slot name="yourItem" :itemInList="item"></slot>
+        </div>
+    </div>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
