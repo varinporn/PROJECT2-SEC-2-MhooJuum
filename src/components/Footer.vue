a405b0999acedc60db1c3b2cea0a0e2bbfb278e9
Varinporn Kiratipattanakorn <varinporn.kira@kmutt.ac.th>
2025-04-06 13:31:05 +0700

    076-[update] responsive Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,22 +2,22 @@
 
 <template>
   <div class="mt-8">
-    <hr class="border-t-2 border-gray-200 mx-auto">
-    <div class="pt-8 pb-12 px-20 flex justify-between">
+    <hr class="border-t-2 border-gray-200 mx-auto" />
+    <div class="pt-8 pb-12 px-6 md:px-20 lg:px-40 flex flex-col md:flex-row md:justify-between space-y-8 md:space-y-0">
       <div>
         <img src="/logo.png" class="h-8" />
         <p class="text-md font-bold py-4 text-[#909cb3]">BestPheamMekPierce Co., Ltd.</p>
-       
       </div>
+
       <div>
-        <div class="flex font-semibold text-sm space-x-32 pr-20">
-          <ul class="space-y-3">
+        <div class="flex flex-col md:flex-row font-semibold text-sm md:space-x-32 lg:space-x-40">
+          <ul class="space-y-3 mb-6 md:mb-0">
             <li class="cursor-pointer text-base">About Us</li>
             <li class="font-normal cursor-pointer">Blog</li>
             <li class="font-normal cursor-pointer">Company News</li>
             <li class="font-normal cursor-pointer">Terms of use</li>
           </ul>
-          <ul class="space-y-3">
+          <ul class="space-y-3 mb-6 md:mb-0">
             <li class="cursor-pointer text-base">Help</li>
             <li class="font-normal cursor-pointer">Contact Us</li>
             <li class="font-normal cursor-pointer">FAQ</li>
@@ -31,6 +31,7 @@
         </div>
       </div>
     </div>
+
     <div class="bg-[#333333] text-center text-white text-md py-3">
       <p>© 2025 BestPheamMekPierce Co., Ltd.</p>
     </div>
